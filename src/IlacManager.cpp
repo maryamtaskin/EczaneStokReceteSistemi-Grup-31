dd9fc83328ca8f88070398c61952a92fbce230b1
Maryam TASKIN <maryamtaskin1818@gmail.com>
2026-01-05 22:56:58 +0300

    Update IlacManager.cpp

--- src/IlacManager.cpp
+++ src/IlacManager.cpp
@@ -2,12 +2,18 @@
 #include <iostream>
 using namespace std;
 
+/*
+ * IlacManager sinifi
+ * Ilaclarin eklenmesi, silinmesi ve stok islemlerini yonetir.
+ */
 IlacManager::IlacManager() {}
 
+// Ilac listesini yeni bir ilac ile gunceller
 void IlacManager::ilacEkle(const Ilac& ilac) {
     ilaclar.push_back(ilac);
 }
 
+// Barkod numarasina gore ilaci listeden siler
 void IlacManager::ilacSil(int barkod) {
     for (int i = 0; i < ilaclar.size(); i++) {
         if (ilaclar[i].getBarkod() == barkod) {
@@ -17,18 +23,21 @@ void IlacManager::ilacSil(int barkod) {
     }
 }
 
+// Belirtilen ilacin stok miktarini artirir
 void IlacManager::stokArtir(int barkod, int miktar) {
     Ilac* ilac = ilacBul(barkod);
     if (ilac != nullptr)
         ilac->stokArtir(miktar);
 }
 
+// Belirtilen ilacin stok miktarini azaltir
 void IlacManager::stokAzalt(int barkod, int miktar) {
     Ilac* ilac = ilacBul(barkod);
     if (ilac != nullptr)
         ilac->stokAzalt(miktar);
 }
 
+// Barkod numarasina gore ilaci bulur
 Ilac* IlacManager::ilacBul(int barkod) {
     for (int i = 0; i < ilaclar.size(); i++) {
         if (ilaclar[i].getBarkod() == barkod)
@@ -37,6 +46,7 @@ Ilac* IlacManager::ilacBul(int barkod) {
     return nullptr;
 }
 
+// Tum ilaclari ekrana listeler
 void IlacManager::listele() {
     cout << "=== ECZANE ILAC LISTESI ===" << endl;
     for (int i = 0; i < ilaclar.size(); i++) {
