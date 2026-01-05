f713d2553f9dc1e2ab3657f2bb391283b2564876
Maryam TASKIN <maryamtaskin1818@gmail.com>
2026-01-05 22:51:10 +0300

    Update Hasta.h
    guncellendi

--- src/Hasta.h
+++ src/Hasta.h
@@ -4,21 +4,29 @@
 #include <string>
 using namespace std;
 
+/*
+ * Hasta sinifi
+ * Hastaya ait temel bilgileri (ad, TC, yas) tutar.
+ * Eczane stok ve recete yonetim sisteminde kullanilir.
+ */
 class Hasta {
 private:
+    // Hasta bilgileri
     string adSoyad;
     string tcKimlik;
     int yas;
 
 public:
+    // Constructor metotlari
     Hasta();
     Hasta(string ad, string tc, int y);
 
-    // Get - Set
+    // Set metotlari: hasta bilgilerini gunceller
     void setAdSoyad(string ad);
     void setTcKimlik(string tc);
     void setYas(int y);
 
+    // Get metotlari: hasta bilgilerini dondurur
     string getAdSoyad() const;
     string getTcKimlik() const;
     int getYas() const;
