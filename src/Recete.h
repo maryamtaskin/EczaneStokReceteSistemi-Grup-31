71d8a6270b60762eca8332f87d3afb20d5dc7347
Maryam TASKIN <maryamtaskin1818@gmail.com>
2026-01-05 22:59:46 +0300

    Update Recete.h
    guncelleme oldu

--- src/Recete.h
+++ src/Recete.h
@@ -8,19 +8,28 @@
 
 using namespace std;
 
+/*
+ * Recete sinifi
+ * Bir hastaya ait recete bilgilerini ve recetede bulunan
+ * ilac listesini tutar.
+ */
 class Recete {
 private:
+    // Receteye ait bilgiler
     int receteNo;
     Hasta hasta;
     vector<Ilac> ilaclar;
 
 public:
+    // Constructor metotlari
     Recete();
     Recete(int no, const Hasta& h);
 
+    // Recete islemleri
     void ilacEkle(const Ilac& ilac);
     void listele() const;
 
+    // Get metotlari
     int getReceteNo() const;
     Hasta getHasta() const;
     vector<Ilac> getIlaclar() const;
